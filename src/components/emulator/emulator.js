7783a4df7f93cd7d08bacfbc10734d97f7374d5c
Daniel Malmqvist <daniel.malmqvist@appland.se>
2020-08-05 12:59:53 +0200

    Bump version WIP

--- src/components/emulator/emulator.js
+++ src/components/emulator/emulator.js
@@ -13,51 +13,185 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import PropTypes from 'prop-types';
-import React, { Component } from 'react';
-import EmulatorWebrtcView from './views/webrtc_view.js';
+import PropTypes from "prop-types";
+import React, { Component } from "react";
+import EmulatorPngView from "./views/simple_png_view.js";
+import EmulatorWebrtcView from "./views/webrtc_view.js";
+import withMouseKeyHandler from "./views/event_handler";
+import JsepProtocol from "./net/jsep_protocol_driver.js";
+import * as Proto from "../../proto/emulator_controller_pb";
+import {
+  RtcService,
+  EmulatorControllerService,
+} from "../../proto/emulator_web_client";
+
+const PngView = withMouseKeyHandler(EmulatorPngView);
+const RtcView = withMouseKeyHandler(EmulatorWebrtcView);
 
 /**
- * An emulator object that displays the screen and sends mouse events via gRPC.
+ * A React component that displays a remote android emulator.
  *
- * The emulator will mount a png, webrtc or fallback view component to display the current state
+ * The emulator will mount a png or webrtc view component to display the current state
  * of the emulator. It will translate mouse events on this component and send them
  * to the actual emulator.
  *
- * The size of this component will be: (width * scale) x (height * scale)
+ * #### Authentication Service
+ *
+ * The authentication service should implement the following methods:
+ *
+ * - `authHeader()` which must return a set of headers that should be send along with a request.
+ * - `unauthorized()` a function that gets called when a 401 was received.
+ *
+ * #### Type of view
+ *
+ * You usually want this to be webrtc as this will make use of the efficient
+ * webrtc implementation. The png view will request screenshots, which are
+ * very slow, and require the envoy proxy. You should not use this for remote emulators.
+ *
+ * Note that chrome will not autoplay the video if it is not muted and no interaction
+ * with the page has taken place. See https://developers.google.com/web/updates/2017/09/autoplay-policy-changes.
+ *
+ * #### Pressing hardware buttons
+ *
+ * This component has a method `sendKey` to sends a key to the emulator.
+ * You can use this to send physical hardwar events to the emulator for example:
+ *
+ * "AudioVolumeDown" - 	Decreases the audio volume.
+ * "AudioVolumeUp"   -	Increases the audio volume.
+ * "Power"	         -  The Power button or key, turn off the device.
+ * "AppSwitch"       -  Should bring up the application switcher dialog.
+ * "GoHome"          -  Go to the home screen.
+ * "GoBack"          -  Open the previous screen you were looking at.
+ *
+ *
  */
-export default class Emulator extends Component {
+class Emulator extends Component {
   static propTypes = {
-    emulator: PropTypes.object, // emulator service
+    /** gRPC Endpoint where we can reach the emulator. */
+    uri: PropTypes.string.isRequired,
+    /** The authentication service to use, or null for no authentication. */
+    auth: PropTypes.object,
+    /** True if the audio should be disabled. This is only relevant when using the webrtc engine. */
+    muted: PropTypes.bool,
+    /** Volume between [0, 1] when audio is enabled. 0 is muted, 1.0 is 100% */
+    volume: PropTypes.number,
+    /** Called upon state change, one of ["connecting", "connected", "disconnected"] */
+    onStateChange: PropTypes.func,
+    /** Called when the audio becomes (un)available. True if audio is available, false otherwise. */
+    onAudioStateChange: PropTypes.func,
+    /** The width of the component */
     width: PropTypes.number,
+    /** The height of the component */
     height: PropTypes.number,
-    scale: PropTypes.number,
-    enableControl: PropTypes.bool,
-    enableFullScreen: PropTypes.bool,
-    turnHost: PropTypes.string,
+    /** The underlying view used to display the emulator, one of ["webrtc", "png"] */
+    view: PropTypes.oneOf(["webrtc", "png"]).isRequired,
+    /** True if polling should be used, only set this to true if you are using the go webgrpc proxy. */
+    poll: PropTypes.bool,
+    /** Callback that will be invoked in case of gRPC errors. */
+    onError: PropTypes.func,
   };
 
   static defaultProps = {
-    width: 576, // The width of the emulator display
-    height: 1024, // The height of the emulator display
-    scale: 1, // Scale factor of the emulator image
+    view: "webrtc",
+    auth: null,
+    poll: false,
+    muted: true,
+    volume: 1.0,
+    onError: (e) => {
+      console.error(e);
+    },
+    onAudioStateChange: (s) => {
+      console.log("emulator audio: " + s);
+    },
+    onStateChange: (s) => {
+      console.log("emulator state: " + s);
+    },
+  };
+
+  components = {
+    webrtc: RtcView,
+    png: PngView,
+  };
+
+  state = {
+    audio: false,
+  };
+
+  constructor(props) {
+    super(props);
+    const { uri, auth, poll, onError } = props;
+    this.emulator = new EmulatorControllerService(uri, auth, onError);
+    this.rtc = new RtcService(uri, auth, onError);
+    this.jsep = new JsepProtocol(this.emulator, this.rtc, poll);
+    this.view = React.createRef();
+  }
+
+  static getDerivedStateFromProps(nextProps, prevState) {
+    if (nextProps.view === "png")
+      return {
+        audio: false,
+      };
+
+    return prevState;
+  }
+
+  /**
+   * Sends the given key to the emulator.
+   *
+   * You can use this to send physical hardware events to the emulator for example:
+   *
+   * "AudioVolumeDown" - 	Decreases the audio volume.
+   * "AudioVolumeUp"   -	Increases the audio volume.
+   * "Power"	         -  The Power button or key, turn off the device.
+   * "AppSwitch"       -  Should bring up the application switcher dialog.
+   * "GoHome"          -  Go to the home screen.
+   * "GoBack"          -  Open the previous screen you were looking at.
+   *
+   * See https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent/key/Key_Values for
+   * a list of valid values.
+   */
+  sendKey = (key) => {
+    var request = new Proto.KeyboardEvent();
+    request.setEventtype(Proto.KeyboardEvent.KeyEventType.KEYPRESS);
+    request.setKey(key);
+    this.jsep.send("keyboard", request);
+  };
+
+  _onAudioStateChange = (s) => {
+    const { onAudioStateChange } = this.props;
+    this.setState({ audio: s }, onAudioStateChange(s));
   };
 
   render() {
-    const { width, height, scale, emulator, enableControl, enableFullScreen, turnHost } = this.props;
-    const styled = { outline: 'none', maxWidth: width * scale };
+    const {
+      width,
+      height,
+      view,
+      poll,
+      muted,
+      onStateChange,
+      onAudioStateChange,
+      onError,
+      volume,
+    } = this.props;
+    const SpecificView = this.components[view] || RtcView;
 
     return (
-      <div tabIndex='1' style={styled}>
-        <EmulatorWebrtcView
-          width={width * scale}
-          height={height * scale}
-          emulator={emulator}
-          enableControl={enableControl}
-          enableFullScreen={enableFullScreen}
-          turnHost={turnHost}
-        />
-      </div>
+      <SpecificView
+        ref={this.view}
+        width={width}
+        height={height}
+        emulator={this.emulator}
+        jsep={this.jsep}
+        onStateChange={this.onStateChange}
+        poll={poll}
+        muted={muted}
+        volume={volume}
+        onError={onError}
+        onAudioStateChange={this._onAudioStateChange}
+      />
     );
   }
 }
+
+export default Emulator;
--- src/components/emulator/net/emulator_status.js
+++ src/components/emulator/net/emulator_status.js
@@ -13,7 +13,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import { Empty } from 'google-protobuf/google/protobuf/empty_pb';
+import { Empty } from "google-protobuf/google/protobuf/empty_pb";
+import { EmulatorControllerService } from "../../../proto/emulator_web_client";
 
 /**
  * Gets the status of the emulator, parsing the hardware config into something
@@ -22,25 +23,49 @@ import { Empty } from 'google-protobuf/google/protobuf/empty_pb';
  * @export
  * @class EmulatorStatus
  */
-export default class EmulatorStatus {
-  constructor(emulator) {
-    this.emulator = emulator;
+class EmulatorStatus {
+  /**
+   * Creates an EmulatorStatus object that can retrieve the status of the running emulator.
+   *
+   * @param {object} uriOrEmulator An emulator controller service, or a URI to a gRPC endpoint.
+   * @param {object} auth The authentication service to use, or null for no authentication.
+   *
+   *  The authentication service should implement the following methods:
+   * - `authHeader()` which must return a set of headers that should be send along with a request.
+   * - `unauthorized()` a function that gets called when a 401 was received.
+   */
+  constructor(uriOrEmulator, auth) {
+    if (uriOrEmulator instanceof EmulatorControllerService) {
+      this.emulator = uriOrEmulator;
+    } else {
+      this.emulator = new EmulatorControllerService(uriOrEmulator, auth);
+    }
     this.status = null;
   }
 
+  /**
+   * Gets the cached status.
+   *
+   * @memberof EmulatorStatus
+   */
   getStatus = () => {
     return this.status;
   };
 
   /**
-   * Update the status of the emulator
+   * Retrieves the current status from the emulator.
    *
    * @param  {Callback} fnNotify when the status is available, returns the retrieved status.
+   * @param  {boolean} cache True if the cache can be used.
    * @memberof EmulatorStatus
    */
-  updateStatus = (fnNotify) => {
+  updateStatus = (fnNotify, cache) => {
     const request = new Empty();
-    this.emulator.getStatus(request).on('data', (response) => {
+    if (cache && this.status) {
+      fnNotify(this.status);
+      return this.status;
+    }
+    this.emulator.getStatus(request, {}, (err, response) => {
       var hwConfig = {};
       const entryList = response.getHardwareconfig().getEntryList();
       for (var i = 0; i < entryList.length; i++) {
@@ -58,10 +83,12 @@ export default class EmulatorStatus {
         vmConfig: {
           hypervisorType: vmConfig.getHypervisortype(),
           numberOfCpuCores: vmConfig.getNumberofcpucores(),
-          ramSizeBytes: vmConfig.getRamsizebytes(),
-        },
+          ramSizeBytes: vmConfig.getRamsizebytes()
+        }
       };
       fnNotify(this.status);
     });
   };
 }
+
+export default EmulatorStatus;
