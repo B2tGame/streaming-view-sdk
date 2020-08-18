dd9ac301cc798b6555e057f133b5dd999c926719
Daniel Fredriksson <daniel.fredriksson@appland.se>
2020-08-18 15:43:07 +0200

    removed the proptypes

--- src/EmulatorController.js
+++ src/EmulatorController.js
@@ -1,10 +1,5 @@
-import PropTypes from 'prop-types';
 import axios from 'axios';
 export default class EmulatorController {
-  static propTypes = {
-    apiEndpoint: PropTypes.string.isRequired,
-    edgeNodeId: PropTypes.string.isRequired,
-  };
   constructor(props) {
     this.apiEndpoint = props.apiEndpoint;
     this.edgeNodeId = props.edgeNodeId;
