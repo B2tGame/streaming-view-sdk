4502b82330092942af49c41ae7383ba8aab56a15
Daniel Fredriksson <daniel.fredriksson@appland.se>
2020-08-19 19:51:53 +0200

    vip

--- src/StreamingController.js
+++ src/StreamingController.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import rp from 'request-promise';
 
 /**
  * Streamingcontroller is responsible for controlling the edge node for example terminate it.
@@ -20,10 +21,44 @@ class StreamingController {
   };
 }
 
+
+const getStatus = (uri, timeOut) =>{
+return new Promise(
+  resolve => {
+    setTimeout(() => resolve(rp.get({uri: uri, json:true})), timeOut);
+  }
+)
+}
+
+const pollStreamStatus = (apiEndpoint, edgeNodeId, maxRetry) => {
+  const loop = retrysLeft => getStatus(`${apiEndpoint}/api/streaming-games/status/${edgeNodeId}`, 1000)
+  .then(result => {
+    if (result.state === 'ready') {
+      console.log("Edge Node controller is 'ready'!");
+      return result
+    }else if (retrysLeft) {
+      return loop(retrysLeft-1)
+    }else{
+      return result;
+    }
+  })
+
+  return loop(maxRetry);
+
+}
+
+
 /**
  * Instanciating the StreamingController
  * @returns {Promise<StreamingController>}
  */
 export default (props) => {
-  return Promise.resolve(new StreamingController(props));
+  return pollStreamStatus(props.apiEndpoint, props.edgeNodeId,10)
+  .then(result => {
+    if(result.state === 'ready'){
+      return new StreamingController({apiEndpoint:result.endpoint, edgeNodeId:props.edgeNodeId})
+    }else{
+      return null;
+    }
+  })
 }
