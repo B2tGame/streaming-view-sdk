268b55f0a1628d0668af23bc14de78c68f57b9ca
Daniel Malmqvist <daniel.malmqvist@appland.se>
2020-12-14 15:00:54 +0100

    WIP

--- src/components/emulator/net/JsepProtocol.js
+++ src/components/emulator/net/JsepProtocol.js
@@ -179,11 +179,11 @@ export default class JsepProtocol {
 
   _handleStart = (signal) => {
     // Emulator passing configuration via start signal
-    const parsedResolution = signal.start.iceServers.configuration.resolution.split('x');
-    StreamingEvent.edgeNode(this.edgeNodeId).emit(StreamingEvent.EMULATOR_CONFIGURATION, {
-      emulatorWidth: parseInt(parsedResolution[0]),
-      emulatorHeight: parseInt(parsedResolution[1])
-    });
+    // const parsedResolution = signal.start.iceServers.configuration.resolution.split('x');
+    // StreamingEvent.edgeNode(this.edgeNodeId).emit(StreamingEvent.EMULATOR_CONFIGURATION, {
+    //   emulatorWidth: parseInt(parsedResolution[0]),
+    //   emulatorHeight: parseInt(parsedResolution[1])
+    // });
 
     signal.start = {
       iceServers: [this.getIceConfiguration()],
