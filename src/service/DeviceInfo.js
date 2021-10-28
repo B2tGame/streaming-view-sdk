f760c40298edde829e683258397931313cf1eb17
Jonathan Gill <jonathan.gill@appland.se>
2021-10-28 08:42:50 +0200

    Autoformat and remove chrome webview

--- src/service/DeviceInfo.js
+++ src/service/DeviceInfo.js
@@ -1,39 +1,28 @@
-import UAParser from 'ua-parser-js'
+import UAParser from 'ua-parser-js';
 
 /**
  * thin wrapper around UAParser, for full info about browsers and devices, see:
  * https://github.com/faisalman/ua-parser-js#methods
  */
 export default class DeviceInfo {
-
-  static supportedBrowsers = [
-    'safari',
-    'mobile safari',
-    'chrome',
-    'chrome headless',
-    // 'chrome webview', // ?
-  ]
+  static supportedBrowsers = ['safari', 'mobile safari', 'chrome', 'chrome headless'];
 
   constructor() {
-    this.userAgentInfo = new UAParser();  
+    this.userAgentInfo = new UAParser();
   }
 
-  /** 
+  /**
    * @returns {UAParser.IResult}
-  */
-
-  get UA () {
-    return this.userAgentInfo.getResult()
+   */
+  get UA() {
+    return this.userAgentInfo.getResult();
   }
 
   getBrowserName() {
-    return this.UA.browser.name ?? ''
+    return this.UA.browser.name ?? '';
   }
 
   isSupportedBrowser() {
-    return DeviceInfo.supportedBrowsers.includes(
-      this.getBrowserName().toLocaleLowerCase()
-    )
+    return DeviceInfo.supportedBrowsers.includes(this.getBrowserName().toLocaleLowerCase());
   }
-
 }
