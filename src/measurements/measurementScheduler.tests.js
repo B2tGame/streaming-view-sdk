c552571b3e256d905e3433b33d62b9ca685fe4b6
Francesco Orsenigo <francesco.orsenigo@appland.se>
2022-08-11 06:45:40 +0000

    Merged in SG-1952-measurementscheduler-tests-shoul (pull request #182)
    Correctly test getPredictedGameExperience polling
    
    * Correctly test getPredictedGameExperience polling
    
    
    Approved-by: Jonathan Gill

--- src/measurements/measurementScheduler.tests.js
+++ src/measurements/measurementScheduler.tests.js
@@ -57,35 +57,41 @@ describe('measurementScheduler', () => {
       onMeasures: () => null,
     });
 
-  describe('getPredictedGameExperiences', () => {
-    it('responds when a measurement is immediately available', async () => {
-      const s = newMeasurementScheduler({
-        axiosGet: (url) => {
-          assert.equal(
-            url,
-            'https://fakeApiEndpoint/api/streaming-games/predicted-game-experience?connectivity-info=%7B%22rttStatsByRegionByTurn%22%3A%7B%7D%7D&deviceInfoId=fakeDeviceInfoId'
-          );
-          return Promise.resolve({ data: { apps: ['someApp'] } });
-        },
-      });
+  describe.only('getPredictedGameExperiences', () => {
+    it('polls only when a measurement is not available', async () => {
+      // How does this test work:
+      //
+      // 1. Create a new scheduler
+      // 2. The first time we call getPredictedGameExperiences, it must poll, because it doesn't have any measurement
+      // 3. The second time we call getPredictedGameExperiences, it must **NOT** poll, and instead use immediately the available measurement
+      //
 
-      const result = await s.getPredictedGameExperiences();
+      const expectedCallUrl =
+        'https://fakeApiEndpoint/api/streaming-games/predicted-game-experience?connectivity-info=%7B%22rttStatsByRegionByTurn%22%3A%7B%7D%7D&deviceInfoId=fakeDeviceInfoId';
+
+      const axiosGet = (url) => {
+        assert.equal(url, expectedCallUrl);
+        return Promise.resolve({ data: { apps: ['someApp'] } });
+      };
+
+      let result;
+
+      // 1. Create a new scheduler, with no measurement available
+      const s = newMeasurementScheduler({ axiosGet });
+
+      // lastMeasure should NOT be available
+      assert(!s.getLastMeasure());
+
+      // 2. Call getPredictedGameExperiences to get the measure
+      result = await s.getPredictedGameExperiences(50);
 
       assert.deepEqual(result, { apps: ['someApp'] });
-    });
 
-    it('polls when connectivity measures take a while to arrive', async () => {
-      const s = newMeasurementScheduler({
-        axiosGet: (url) => {
-          assert.equal(
-            url,
-            'https://fakeApiEndpoint/api/streaming-games/predicted-game-experience?connectivity-info=%7B%22rttStatsByRegionByTurn%22%3A%7B%7D%7D&deviceInfoId=fakeDeviceInfoId'
-          );
-          return Promise.resolve({ data: { apps: ['someApp'] } });
-        },
-      });
+      // lastMeasure SHOULD be available
+      assert(!!s.getLastMeasure());
 
-      const result = await s.getPredictedGameExperiences(10);
+      // 3. Call getPredictedGameExperiences again with a pollingInterval guaranteed to send the test in timeout
+      result = await s.getPredictedGameExperiences(99999999);
 
       assert.deepEqual(result, { apps: ['someApp'] });
     });
