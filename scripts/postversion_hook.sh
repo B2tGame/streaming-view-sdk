d0913ad844a608d27218c6a87413a5f6d0495696
Francesco Orsenigo <francesco.orsenigo@appland.se>
2022-12-30 09:14:56 +0100

    maybe

--- scripts/postversion_hook.sh
+++ scripts/postversion_hook.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 set -e
 
+# This is run to ensure that build-info.json in build/ is up to date and that build/ is there
+npm run build
+
 cp package.json build/package.json
 
-cp .npmrc build/.npmrc 2>/dev/null || true
+# TODO: remove this once we know that the build works
+# npmrc is added by build.yml to contain the npm-token to be able to push to npm
+# However, on local it is not available
+#cp .npmrc build/.npmrc 2>/dev/null || true
 
-cp src/controllers/build-info.json build/src/controllers/build-info.json
