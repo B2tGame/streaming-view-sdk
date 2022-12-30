7756e1f89446a2813d8387a74227d4101f968beb
Francesco Orsenigo <francesco.orsenigo@onmo.com>
2022-12-30 12:28:17 +0000

    Merged in SG-1844-buildinfo.json-should-be-updated (pull request #215)
    Clean up node scripts
    
    * Clean up node scripts
    
    * maybe
    
    * fix
    
    * fix source maps
    
    
    Approved-by: Jonathan Gill

--- scripts/build.sh
+++ scripts/build.sh
@@ -12,4 +12,3 @@ tsc --build
 # Generate/copy all other files
 echo '@applandstream/streaming-view-sdk' > build/README.md
 cp package.json build/
-cp src/controllers/service/neural-network-models/b540f780-9367-427c-8b05-232cebb9ec49.json build/controllers/service/neural-network-models/
